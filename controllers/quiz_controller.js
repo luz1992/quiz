92d715bfca41ec8cf35e28fc13df6d6c14109294
Olalla <o.otero@alumnos.upm.es>
2015-05-12 17:29:21 +0200

    version 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,8 +3,8 @@ var models = require ('../models/models.js')
 
 exports.show = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show.ejs', {quiz: quiz})
-	});
+		res.render('quizes/show', {quiz: quiz})
+	})
 };
 	
 //GET /quizes
@@ -17,10 +17,10 @@ exports.index = function(req, res){
 exports.answer = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
 		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer.ejs',
+			res.render('quizes/answer',
 				{quiz: quiz, respuesta: 'Correcto'});
 		} else{
-			res.render('quizes/answer.ejs',
+			res.render('quizes/answer',
 				{quiz: quiz, respuesta: 'Incorrecto'});
 		}
 	})
