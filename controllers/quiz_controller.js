2d177cf74acfbc301cadc9858fd8dfc26e53023a
Olalla <o.otero@alumnos.upm.es>
2015-05-18 11:12:01 +0200

    borrarPreguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,7 @@
 //GET /quizes/:id
 var models = require ('../models/models.js');
 
-//Autoload - factoriza el código si ruta incluye :quizId
+// Autoload :id
 
 exports.load = function(req, res, next, quizId) {
 	models.Quiz.find(quizId).then(
@@ -9,22 +9,23 @@ exports.load = function(req, res, next, quizId) {
 			if(quiz){
 				req.quiz = quiz;
 				next();
-			}else{
-				next(new Error('No existe quizId=' + quizId));}
+			
+			 } else{next(new Error('No existe quizId=' + quizId))}
 		}
-	).catch(function(error) { next(error);});
+	).catch(function(error){next(error)});
 };
-
+// GET /quizes/:id
 exports.show = function(req, res) {
-	
-	res.render('quizes/show', {quiz: req.quiz});
-};
+
+res.render('quizes/show', { quiz: req.quiz, errors: []});
+}; // req.quiz: instancia de quiz cargada con autoload
+
 	
 //GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then( function(quizes) {
-		res.render('quizes/index',{quizes: quizes});
-	}).catch(function(error) { next(error);})
+		res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+	} ).catch(function(error){next(error)});
 };
 //GET /quizes/:id/answer
 exports.answer = function(req, res) {
@@ -33,23 +34,78 @@ exports.answer = function(req, res) {
 		if (req.query.respuesta === req.quiz.respuesta){
 			resultado = 'Correcto';
 		}
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+		 res.render(
+ 'quizes/answer',
+ { quiz: req.quiz,
+ respuesta: resultado,
+ errors: []
+ }
+ );
 
 };
-//GET /quizes/new
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+ var quiz = req.quiz; // req.quiz: autoload de instancia de quiz
+
+res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+ req.quiz.pregunta = req.body.quiz.pregunta;
+ req.quiz.respuesta = req.body.quiz.respuesta;
+
+ req.quiz
+.validate()
+ .then(
+ function(err){
+ if (err) {
+res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+ } else {
+ req.quiz // save: guarda campos pregunta y respuesta en DB
+ .save( {fields: ["pregunta", "respuesta"]})
+ .then( function(){ res.redirect('/quizes');});
+ } // Redirección HTTP a lista de preguntas (URL relativo)
+ }
+).catch(function(error){next(error)});
+
+};
+// GET /quizes/new
 exports.new = function(req, res) {
-	var quiz = models.Quiz.build(	//crea objeto quiz
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
-	);
-	res.render('quizes/new', {quiz:quiz});
-};
-exports.create = function(req, res) {
-	var quiz = models.Quiz.build( req.body.quiz);
+var quiz = models.Quiz.build( // crea objeto quiz 
+{pregunta: "Pregunta", respuesta: "Respuesta"}
+ );
 
-//guarda en DB los campos pregunta y respuesta de quiz
-quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-	res.redirect('/quizes');
-})	//Redireccion HTTP (URL relativo) lista de preguntas
-		
+res.render('quizes/new', {quiz: quiz, errors: []});
 };
+
+// POST /quizes/create
+exports.create = function(req, res) {
+ var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+ quiz
+.validate()
+.then(
+function(err){
+ if (err) {
+res.render('quizes/new', {quiz: quiz, errors: err.errors});
+ } else {
+quiz // save: guarda en DB campos pregunta y respuesta de quiz
+ .save({fields: ["pregunta", "respuesta"]})
+.then( function(){ res.redirect('/quizes')})
+} // res.redirect: Redirección HTTP a lista de preguntas
+}
+).catch(function(error){next(error)});
+
+}; 
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+ req.quiz.destroy().then( function() {
+ res.redirect('/quizes');
+ }).catch(function(error){next(error)});
+};
+
+// console.log("req.quiz.id: " + req.quiz.id);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 //Autoload de comandos con :quizId
@@ -13,8 +13,11 @@ router.param( 'quizId', quizController.load); //autoload :quizId
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
-router.get('/quiezes/new',					quizController.new);
-router.post('/quiezes/create',				quizController.create);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/author', function(req,res){
 	res.render('author', { title: 'Quiz' });
