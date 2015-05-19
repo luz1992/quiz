5161d618db4c4024e2c48f810f6de2f23c3eab09
Olalla <o.otero@alumnos.upm.es>
2015-05-19 13:02:29 +0200

    HTTPS

--- app.js
+++ app.js
@@ -4,41 +4,40 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var partials = require ('express-partials');
+var partials = require('express-partials');
 var methodOverride = require('method-override');
 var session = require('express-session');
-
 var routes = require('./routes/index');
 var app = express();
+var https = require('https');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(partials());
 // uncomment after placing your favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded());
 app.use(cookieParser('Quiz 2015'));
 app.use(session());
-
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 // Helpers dinamicos:
 app.use(function(req, res, next) {
-
- // guardar path en session.redir para despues de login
-if (!req.path.match(/\/login|\/logout/)) {
+// si no existe lo inicializa
+if (!req.session.redir) {
+req.session.redir = '/';
+}
+// guardar path en session.redir para despues de login
+if (!req.path.match(/\/login|\/logout|\/user/)) {
 req.session.redir = req.path;
 }
-
- // Hacer visible req.session en las vistas
- res.locals.session = req.session;
- next();
+// Hacer visible req.session en las vistas
+res.locals.session = req.session;
+next();
 });
-
-
 app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
