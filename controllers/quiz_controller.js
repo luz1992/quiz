c7b4b077742a4864cf2e6fa721a52d31201e6904
Olalla <o.otero@alumnos.upm.es>
2015-05-20 18:34:10 +0200

    Quiz_2015

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,9 +45,14 @@ res.render('quizes/show', { quiz: req.quiz, errors: []});
 	
 //GET /quizes
 exports.index = function(req, res) {
+		var options = {};
+	if(req.user){ // req.user es creado por autoload de usuario
+				  // si la ruta lleva el párametro .quizId
+		options.where = {UserId: req.user.id}	
+	}
 var busqueda = req.query.search;
  if(busqueda== null){
- models.Quiz.findAll().then(
+ models.Quiz.findAll(options).then(
  function(quizes){
  res.render('quizes/index', { quizes: quizes, errors: []});
  }
--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post('/user', userController.create);    // registrar usuario
 router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userController.ownershipRequired, userController.edit);
 router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.update);
 router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.destroy);
-
+router.get('/user/:userId(\\d+)/quizes', quizController.index); // Ver las preguntas de un usuario
 
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
